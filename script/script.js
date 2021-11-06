30bf462cfe498523a10c373f8b68924d515efc3b
Dominic Bell <86816915+dobell733@users.noreply.github.com>
2021-11-05 21:59:32 -0700

    Add JS file for dropdown project image

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function togglePicture() {
+    let element = document.getElementById("DFHH");
+    if (element.style.display == "block") {
+        element.style.display = "none" ;
+    } else {
+        element.style.display = "block";
+    }
+}
