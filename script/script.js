d55429aa0c864635977ae8f1b40d51ffaf448889
dobell733 <dominicbell30@outlook.com>
2023-05-24 22:58:47 -0700

    change toggle functions for deployment purposes

--- script/script.js
+++ script/script.js
@@ -1,5 +1,34 @@
-function togglePicture(id) {
-    let element = document.getElementById(id);
+// I would prefer for these to be one function with an input parameter, but it wouldn't play nice with deploying on Github.
+
+function toggleDFHH() {
+    let element = document.getElementById("DFHH");
+    if (element.style.display == "block") {
+        element.style.display = "none" ;
+    } else {
+        element.style.display = "block";
+    }
+}
+
+function toggleHash() {
+    let element = document.getElementById("hash_map");
+    if (element.style.display == "block") {
+        element.style.display = "none" ;
+    } else {
+        element.style.display = "block";
+    }
+}
+
+function toggleSet() {
+    let element = document.getElementById("set_graph");
+    if (element.style.display == "block") {
+        element.style.display = "none" ;
+    } else {
+        element.style.display = "block";
+    }
+}
+
+function toggleData() {
+    let element = document.getElementById("data_ball");
     if (element.style.display == "block") {
         element.style.display = "none" ;
     } else {
